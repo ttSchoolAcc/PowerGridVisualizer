ae286b8f88e3adb6484c203fa2f3905d43f2381a
ttSchoolAcc <ttSchoolAcc>
2024-10-19 13:53:54 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import solarPanel from './Graphics/SolarPanel.png';
+import windTurbine from './Graphics/WindTurbine.png';
+
 function App() {
+
+  //let solarNum = 0;
+  const [solarNum, setCount] = useState(0);
+
+  function handleSubmit(e) 
+  {
+    e.preventDefault();
+    setCount(solarNum + 1);
+  }
+
+  function removeSolarNum(e) 
+  {
+    if(solarNum > 0)
+    {
+      e.preventDefault();
+      setCount(solarNum - 1);
+    }
+  }
+
+  function addWindNum(e) 
+  {
+    if(solarNum > 0)
+    {
+      e.preventDefault();
+      setCount(solarNum - 1);
+    }
+  }
+
+  function removeWindNum(e) 
+  {
+    if(solarNum > 0)
+    {
+      e.preventDefault();
+      setCount(solarNum - 1);
+    }
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div class="mainpage-columns-parent">
+
+        <div class="mainpage-columns" style={{flex: '15%'}}>
+          <h2>Energy Sources</h2>
+
+          <button onClick={handleSubmit}>
+            <img src={solarPanel} alt="Solar Panel" class="power-source-image"/>
+          </button>
+
+          <button onClick={handleSubmit}>
+            <img src={windTurbine} alt="Wind Turbine" class="power-source-image"/>
+          </button>
+
+          <button onClick={handleSubmit}>
+            <img src={solarPanel} alt="Solar Panel" class="power-source-image"/>
+          </button>
+
+        </div>
+
+        <div class="mainpage-columns" style={{flex: '40%'}}>
+          <h2>Grid</h2>
+
+          <div class="power-grid">
+
+            <button onClick={removeSolarNum}>
+              <img src={solarPanel} alt="Solar Panel" class="power-source-image"/>
+            </button>
+            <p>x{solarNum}</p>
+            
+            <button>
+              <img src={windTurbine} alt="Wind Turbine" class="power-source-image"/>
+            </button>
+            <p>x{solarNum}</p>
+
+          </div>
+        </div>
+
+        <div style={{flex: '45%'}}>
+          <div class="mainpage-columns" style={{height: '400px'}}>
+            <h2>Homes</h2>
+          </div>
+
+          <div class="mainpage-columns" style={{height: '400px'}}>
+            <h2>Stats</h2>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
